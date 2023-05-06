ab98620874608d58b2d6143bfb61e757ee6cfb0c
Janyk Steenbeek <me@janyk.dev>
2023-05-07 00:20:49 +0200

    Added icons, fine-grained content script match URL, clean-up

--- background.js
+++ background.js
@@ -1,9 +1,8 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.type === "getAuthToken") {
-      chrome.identity.getAuthToken({ interactive: true }, (token) => {
-        sendResponse(token);
-      });
-      return true; // Need to return true to indicate that we're sending a response asynchronously
-    }
-  });
-  
+  if (request.type === "getAuthToken") {
+    chrome.identity.getAuthToken({ interactive: true }, (token) => {
+      sendResponse(token);
+    });
+    return true; // Need to return true to indicate that we're sending a response asynchronously
+  }
+});
--- content.js
+++ content.js
@@ -16,13 +16,14 @@ function fetchCalendarEvents(token, calendarId, startDate, endDate) {
     });
 }
 
+// Mark busy dates on the website's date selection UI
 async function markBusyDates() {
     const dateblocks = document.querySelectorAll(".date");
     const startDate = new Date(dateblocks[0].getAttribute("data-startdate")).toISOString();
     const endDate = new Date(dateblocks[dateblocks.length - 1].getAttribute("data-startdate")).toISOString();
 
+    // Get the authentication token from the Chrome extension
     chrome.runtime.sendMessage({ type: "getAuthToken" }, async (token) => {
-        console.log(token);
         const response = await fetch("https://www.googleapis.com/calendar/v3/users/me/calendarList", {
             headers: {
                 Authorization: `Bearer ${token}`,
@@ -32,36 +33,45 @@ async function markBusyDates() {
         const calendarIds = data.items.map((item) => item.id);
         const events = (await Promise.all(calendarIds.map((calendarId) => fetchCalendarEvents(token, calendarId, startDate, endDate)))).flat();
 
+        // Loop through each date block and check if there's an event on that date
         dateblocks.forEach((dateblock) => {
             const startDate = new Date(dateblock.getAttribute("data-startdate"));
             if (!events) return;
-            const eventForDate = events.filter((event) => {
+            const overlappingEvents = events.filter((event) => {
                 if(event.start == undefined || event.end == undefined) return false;
                 let eventStart = event.start.dateTime || event.start.date;
                 let eventEnd = event.end.dateTime || event.end.date;
 
+
+                // Check if the event overlaps with the current date block
                 try {
                     eventStart = new Date(Date.parse(eventStart));
                     eventEnd = new Date(Date.parse(eventEnd));
-                    return startDate != eventEnd && eventStart <= startDate && startDate < eventEnd;
+
+                    // Check if it's a whole-day event and adjust the end date accordingly
+                    if (event.start.date && !event.start.dateTime) {
+                        eventEnd.setDate(eventEnd.getDate() - 1);
+                    }
+
+                    return eventStart <= startDate && startDate < eventEnd; 
                 } catch (e) {
-                    console.error(e);
+                    console.error("Error parsing event date:", e);
                     return false;
                 }
 
                 return false;
             });
 
-            console.log(eventForDate);
-            if (eventForDate.length > 0) {
-                console.log(dateblock.parentNode.querySelector("[data-value=no]"));
+            // If there's an event on the date, mark it as busy
+            if (overlappingEvents.length > 0) {
                 dateblock.parentNode.querySelector("[data-value=no]").click();
-                dateblock.insertAdjacentHTML("beforeend", `<div class="dateblock__busy">${eventForDate[0].summary}</div>`);
+                dateblock.insertAdjacentHTML("beforeend", `<div class="dateblock__busy">${overlappingEvents[0].summary}</div>`);
             }
         });
     });
 }
 
+// Wait for the date blocks to load on the page
 function waitForDateBlocks() {
     return new Promise((resolve) => {
         const interval = setInterval(() => {
@@ -77,9 +87,9 @@ function waitForDateBlocks() {
 async function main() {
     await waitForDateBlocks();
 
+    // Add custom styles for the busy date indicator
     const style = document.createElement("style");
-    style.innerHTML = `
-    .dateblock__busy {
+    style.innerHTML = `.dateblock__busy {
         background-color: #ff0000;
         color: #fff;
         font-size: 12px;
@@ -93,6 +103,7 @@ async function main() {
     markBusyDates();
 }
 
+// Initialize the script with a delay to ensure the page has loaded
 setTimeout(async () => {
     const button = document.querySelector("#nav_next");
     button.click();
